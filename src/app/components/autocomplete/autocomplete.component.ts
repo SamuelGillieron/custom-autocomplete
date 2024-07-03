33f8b07cc17f87b0e315da664d95b4f91ee634b2
Samuel GILLIERON <samuel.gillieron@softcom.pro>
2024-07-03 17:05:23 +0200

    DIRTY: Base autocomplete

--- src/app/components/autocomplete/autocomplete.component.ts
+++ src/app/components/autocomplete/autocomplete.component.ts
@@ -1,21 +1,45 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {CocktailServiceService} from '../../shared/services/cocktail-service.service';
+import {debounce, debounceTime, fromEvent, Observable, switchMap, tap} from 'rxjs';
+import {Cocktail} from '../../shared/models/cocktail';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-autocomplete',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './autocomplete.component.html',
   styleUrl: './autocomplete.component.scss'
 })
-export class AutocompleteComponent {
+export class AutocompleteComponent implements AfterViewInit{
+
+  @Input()
+  placeholder : string = 'Search'
+
+  @ViewChild('autocompleteInput')
+  input: ElementRef = null!
+  filteredInput$: Observable<Cocktail[]> = null!;
+  inputValue: string = '';
+
 
   constructor(private cocktailService : CocktailServiceService) {
-    cocktailService.searchCocktail("margarita").subscribe(
-      cocktail => {
-        console.log(cocktail)
-        console.log("DrinkName",cocktail.map(c => c.strDrink))
-      }
-    )
   }
+
+  ngAfterViewInit(): void {
+        this.filteredInput$ = fromEvent(this.input.nativeElement, 'keyup').pipe(
+          tap(e => console.log((e as any).target.value)),
+          debounceTime(300),
+          switchMap(e => this.cocktailService.searchCocktail((e as any).target.value))
+        )
+
+    this.filteredInput$.subscribe(cocktail =>  {
+      console.log(cocktail)
+      if(cocktail)
+        console.log("DrinkName",cocktail.map(c => c.strDrink))
+    })
+    }
+
+
 }
